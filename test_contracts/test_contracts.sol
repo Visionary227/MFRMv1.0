7dbd08ec2414b91daad7ceade267f946a5448e6b
JC <jdcommons@gmail.com>
2020-12-15 22:30:21 -0800

    add folder for test contracts
    folder test_contracts contains code of all contracts deployed to Kovan
    file test_contracts.sol contains Etherscan links to said contracts

---
+++ test_contracts/test_contracts.sol
@@ -0,0 +1,14 @@
+//openzepERC20_fountain "kovan cookies" / "COOK"
+//https://kovan.etherscan.io/address/0x48c075de556cd05e2ecc2781aefcb21f014b32cf
+
+
+//Uniswap V2 pair ETH-COOK
+//https://kovan.etherscan.io/address/0x62378c443d221593fb8e45175797bbb6a0d0b0c9
+
+
+//openzep_MFRM_simple
+//https://kovan.etherscan.io/address/0x916d83c3ae414f1c0550e30ff94e8204cace2efb
+
+
+//MFRMMasterChef
+//https://kovan.etherscan.io/address/0x4d4183708f0faC88D22dd304e1197E5899556Fa7
